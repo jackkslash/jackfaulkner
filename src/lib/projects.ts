ccf9219f5de6c218927b3ec9f9083a0710f06160
jackkslash <jackfaulknerbis@hotmail.co.uk>
2025-04-20 23:42:22 +0100

    Project and Social data added

---
+++ src/lib/projects.ts
@@ -0,0 +1,58 @@
+export interface Project {
+    title: string;
+    description: string;
+    important: boolean;
+    href: string;
+  }
+  
+  export interface Link {
+    title: string;
+    href: string;
+  }
+  
+  export const ProjectData: Project[] = [
+    {
+      title: "ChatterBox",
+      description: "Chatterbox is a AI-powered assistant using multiple AI models and web search capabilities.",
+      important: true,
+      href: "https://github.com/jackkslash/ChatterBox",
+    },
+    {
+      title: "Huddle",
+      important: true,
+      description: " A Modern Fullstack Social News Website and Forum",
+      href: "https://github.com/jackkslash/huddl",
+    },
+    {
+      title: "Tombot",
+      description:
+        "Discord bot that watches the ethereum block for user submitted wallets",
+      important: true,
+      href: "https://github.com/jackkslash/tombot",
+    },
+    {
+      title: "MSAI",
+      description: "Meme reverse image search using vector database and AI.",
+      important: true,
+      href: "https://github.com/jackkslash/MSAI",
+    },
+  ];
+  
+  export const Links: Link[] = [
+    {
+      title: "github",
+      href: "https://github.com/jackkslash",
+    },
+    {
+      title: "twitter",
+      href: "https://twitter.com/jackkslash",
+    },
+    {
+      title: "linkedin",
+      href: "https://www.linkedin.com/in/jackaaronfaulkner/",
+    },
+    {
+      title: "email",
+      href: "mailto:jackfaulknerbis@hotmail.co.uk",
+    },
+  ];
